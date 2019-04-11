7dc613c5a6a436e027e5295cef1ee3a5a873f318
dkrey <dirk.kreyenberg@gmail.com>
2019-04-11 18:10:56 +0200

    put Banktheft in separate file

---
+++ tests/testWait.asm
@@ -0,0 +1,28 @@
+
+#import "testHeader.asm"
+
+main:
+
+  sei
+loop:
+
+  jsr addDelay
+  :update_score score : #1
+  :print_score score
+
+  jmp loop
+
+
+score:
+  .word 03
+
+result:
+  .word $0000
+
+.pseudocommand update_score score : value {
+  :add16 score : value : score
+}
+
+.pseudocommand print_score score {
+  :int16_to_hex_str score : SCREEN + 40*12 + 17
+}
