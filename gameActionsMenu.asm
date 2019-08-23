7c11b214a434ea4e715e024e6c7fb71e68fdc099
Dirk <dirk.kreyenberg@gmail.com>
2019-08-23 22:27:13 +0200

    added a note

--- gameActionsMenu.asm
+++ gameActionsMenu.asm
@@ -19,3 +19,16 @@ gameActionsMenu:
     jsr Print_text
     jsr Wait_for_key
     rts
+
+/*
+Bandenkrieg
+
+Revolverhelden bzw. Wächter: max. 9 pro Seite
+würfeln von 1-10
+Bei Informanten an der Untergrenze +1
+
+Abfrage von Disastern: Wenn genug Bestechungen: Würfeln für mehr Angreifer bzw. Wächter
+dann max 3 Revolverhelden oder 3 Wächter extra, ebenfalls per Zufall
+
+
+ */
